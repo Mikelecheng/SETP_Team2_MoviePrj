cbeff2c3aed4843ae965e53c2f3cb34787cb47a9
Mikelecheng <lecheng520@yahoo.com>
2022-08-11 20:00:37 +0800

    Update add_theatre_2.php
    correction of label name

--- Movie/theatre/pages/add_theatre_2.php
+++ Movie/theatre/pages/add_theatre_2.php
@@ -47,7 +47,7 @@ include('header.php');
                     <td><?php echo $theatre['state'];?></td>
                 </tr>
                 <tr>
-                    <td>Pin</td>
+                    <td>Postal Code</td>
                     <td><?php echo $theatre['pin'];?></td>
                 </tr>
             </table>
